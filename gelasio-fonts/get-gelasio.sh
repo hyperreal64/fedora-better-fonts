73c2ad751661efc184ecf4a58b777cabecf4ba63
Jeffrey Serio <23226432+hyperreal64@users.noreply.github.com>
2023-02-03 10:33:16 -0600

    Update get-gelasio.sh

--- gelasio-fonts/get-gelasio.sh
+++ gelasio-fonts/get-gelasio.sh
@@ -7,7 +7,7 @@ TMPDIR=$(mktemp -d --tmpdir=/var/tmp getgelasio-XXXXXXXXXX)
 umask 022
 pushd "$TMPDIR"
 
-wget -N -O $ARCHIVE.zip https://fonts.google.com/download?family=Gelasio
+wget -N -O $ARCHIVE.zip "https://fonts.google.com/download?family=Gelasio"
 unzip $ARCHIVE.zip -d $ARCHIVE
 tar -cvJf "$ARCHIVE.tar.xz" $ARCHIVE
 
